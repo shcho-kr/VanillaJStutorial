0eb5e139c94fe80829bb49439927a71dc067944d
shcho-kr <103431041+shcho-kr@users.noreply.github.com>
2023-03-04 23:22:50 +0900

    event1

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const a = 5;
-const b = 8;
-let myName = "sungho";
+const title = document.querySelector("div.hello:first-child h1");
 
+function handleTitleClick() {
+    title.style.color = "blue";
+    console.log("title was clicked!");
+}
 
-
+title.addEventListener("click", handleTitleClick);
 
